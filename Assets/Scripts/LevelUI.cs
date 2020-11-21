a4391a17e863fbc14b906953b750ca8a127c1ef3
ZigZagZap10 <58115931+ZigZagZap10@users.noreply.github.com>
2020-11-21 15:11:35 -0600

    Initial commit

---
+++ Assets/Scripts/LevelUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUI : MonoBehaviour
+{
+    // Level components
+    public GameObject Level;
+    public float CurrentRotation;
+    public float speedRotate;
+    public bool RotateLeft;
+    public bool RotateRight;
+
+    private void Start()
+    {
+        RotateLeft = false;
+        RotateRight = false;
+    }
+
+    private void Update()
+    {
+        if (RotateLeft == true)
+        {
+            Level.transform.Rotate(Vector3.forward * speedRotate * -1 * Time.deltaTime);
+        }
+
+        if (RotateRight == true)
+        {
+            Level.transform.Rotate(Vector3.forward * speedRotate * Time.deltaTime);
+        }
+    }
+
+    public void RotateObjectLeft()
+    {
+        RotateLeft = true;
+    }
+
+    public void RotateObjectRight()
+    {
+        RotateRight = true;
+    }
+
+}
