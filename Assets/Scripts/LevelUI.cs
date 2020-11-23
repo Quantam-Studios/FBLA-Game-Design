5f7a37f24e20df2b1f7f485163799d4fcfbf380c
ZigZagZap10 <58115931+ZigZagZap10@users.noreply.github.com>
2020-11-23 14:01:43 -0600

    Push 2

--- Assets/Scripts/LevelUI.cs
+++ Assets/Scripts/LevelUI.cs
@@ -1,43 +1,126 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelUI : MonoBehaviour
 {
-    // Level components
+
+    //Level attributes
     public GameObject Level;
-    public float CurrentRotation;
+
+    //Rotation
+    private Quaternion CurrentRotation = new Quaternion(0f, 0f, 0f, 1f);
     public float speedRotate;
-    public bool RotateLeft;
-    public bool RotateRight;
+    private Quaternion target = new Quaternion(0f, 0f, 0f, 1f);
+
+    private Quaternion left = new Quaternion(0f, 0f, -0.7071068f, 0.7071068f);
+    private Quaternion right = new Quaternion(0f, 0f, 0.7071068f, 0.7071068f);
+    public Button LeftButton;
+    public Button RightButton;
+
+    //Player attributes
+    public GameObject Player;
+    private Rigidbody2D rb;
+
+    //Counters
+    public Text MovesText;
+    public float Moves;
+    public Text ItemsText;
+    public static float Items;
+    public float TotalItems;
+
+    //End level
+    public GameObject EndLevelMenu;
+    public Text TotalMovesText;
 
     private void Start()
     {
-        RotateLeft = false;
-        RotateRight = false;
+        Items = 0;
+        Moves = 0;
+        rb = Player.GetComponent<Rigidbody2D>();
+        rb.gravityScale = 1;
+        MovesText.text = "Moves: 0";
+        ItemsText.text = "0 / " + TotalItems;
+        EndLevelMenu.SetActive(false);
     }
 
-    private void Update()
+    IEnumerator RotateObjectLeft()
     {
-        if (RotateLeft == true)
+        bool ltarget = false;
+        target = CurrentRotation * left;
+        Moves += 1;
+        //Start rotating
+        while (ltarget == false)
         {
-            Level.transform.Rotate(Vector3.forward * speedRotate * -1 * Time.deltaTime);
+            RightButton.interactable = false;
+            LeftButton.interactable = false;
+            rb.gravityScale = 0;
+            Level.transform.rotation = Quaternion.RotateTowards(CurrentRotation, target, speedRotate * Time.deltaTime);
+            if (CurrentRotation == target)
+            {
+                RightButton.interactable = true;
+                LeftButton.interactable = true;
+                rb.gravityScale = 1;
+                ltarget = true;
+                Level.transform.rotation = Quaternion.Slerp(CurrentRotation, target, 1f);
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateObjectRight()
+    {
+        bool rtarget = false;
+        target = CurrentRotation * right;
+        Moves += 1;
+        //Start rotating
+        while (rtarget == false)
+        {
+            RightButton.interactable = false;
+            LeftButton.interactable = false;
+            rb.gravityScale = 0;
+            Level.transform.rotation = Quaternion.RotateTowards(CurrentRotation, target, speedRotate * Time.deltaTime);
+            if (CurrentRotation == target)
+            {
+                RightButton.interactable = true;
+                LeftButton.interactable = true;
+                rb.gravityScale = 1;
+                rtarget = true;
+                Level.transform.rotation = Quaternion.Slerp(CurrentRotation, target, 1f);
+            }
+            yield return null;
         }
 
-        if (RotateRight == true)
+    }
+
+    void Update()
+    {
+        CurrentRotation = Level.transform.rotation;
+        MovesText.text = "Moves: " + Moves;
+        ItemsText.text = Items + " / " + TotalItems;
+
+        if (Items == TotalItems)
         {
-            Level.transform.Rotate(Vector3.forward * speedRotate * Time.deltaTime);
+            EndLevelMenu.SetActive(true);
+            TotalMovesText.text = "Total Moves: " + Moves;
         }
     }
 
-    public void RotateObjectLeft()
+    //Functions called onClick()
+    public void RotateLeft()
     {
-        RotateLeft = true;
+        StartCoroutine(RotateObjectLeft());
     }
 
-    public void RotateObjectRight()
+    public void RotateRight()
     {
-        RotateRight = true;
+        StartCoroutine(RotateObjectRight());
     }
 
+    public void Retry()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject other = col.gameObject;
+        if (col.gameObject.tag == "Item")
+        {
+            LevelUI.Items += 1;
+            Destroy(other);
+        }
+    }
+}
